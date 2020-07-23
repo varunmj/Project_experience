1484d6a16a3a031f9ed916529bda8a46b089433b
Varun Mohankumar <varunmj@users.noreply.github.com>
2020-07-24 02:51:03 +0530

    new file added(index.py)

--- index.py
+++ index.py
@@ -1,1 +1,13 @@
-print("new to github")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def print_my_name():
+
+    for i in range(0,12):
+        print("Hello, Varun MJ")
+
+    return
+
+if __name__ =="__main__":
+    print_my_name()

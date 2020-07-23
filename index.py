92bcc1a1874a673d1bf4deefeb07e537cf07175d
Varun Mohankumar <varunmj@users.noreply.github.com>
2020-07-24 03:27:42 +0530

    logic added

--- index.py
+++ index.py
@@ -5,9 +5,13 @@ import matplotlib.pyplot as plt
 n = int(input())
 
 def print_my_name(n):
+    array =[]
 
     for i in range(0,n):
         print("Hello, Varun MJ")
+        array.append(i)
+        print("the code is executed for the "+ str(i) +"th time")
+        print(array[i])
 
     return n
 

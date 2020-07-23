6ca8814880d3fd36475659132d6be2ce40f40eef
Varun Mohankumar <varunmj@users.noreply.github.com>
2020-07-24 02:56:30 +0530

    input added

--- index.py
+++ index.py
@@ -2,12 +2,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def print_my_name():
+n = input()
 
-    for i in range(0,12):
+def print_my_name(n):
+
+    for i in range(0,n):
         print("Hello, Varun MJ")
 
-    return
+    return n
 
 if __name__ =="__main__":
     print_my_name()
